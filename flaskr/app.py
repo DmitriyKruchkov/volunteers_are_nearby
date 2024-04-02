a4d7ad8f5112be54795e8237e7d86f2fce296637
Irina <2003.irisha@mail.ru>
2024-04-02 20:33:19 +0300

    Создано описание проекта

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, redirect
 from flask_login import LoginManager, login_user, login_required, logout_user
-
 from data import db_session
 from config import SECRET_KEY, HOST, PORT, DEBUG
 from form.loginform import LoginForm
