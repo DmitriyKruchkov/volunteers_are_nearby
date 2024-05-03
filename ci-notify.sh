8b4d982c1d9dc74911bbc5981c6cb35dde3ad1ff
DmitriyKruchkov <kruchkovdmitriy48@yandex.ru>
2024-05-04 01:27:46 +0300

    fixing pipeline
    Signed-off-by: DmitriyKruchkov <kruchkovdmitriy48@yandex.ru>

--- ci-notify.sh
+++ ci-notify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 TIME="10"
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-TEXT="Deploy status:%0A $(cat $1)%0A%0ABranch:+$1%0AProject:+$2"
+TEXT="Deploy status:%0A $(cat $1)%0A%0ABranch:+$2%0AProject:+$3"
 curl -s --max-time $TIME -d "chat_id=$TELEGRAM_CHAT_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
