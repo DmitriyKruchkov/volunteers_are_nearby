2f3309c9485127e6734cf7a2954bfbdfb3079f21
DmitriyKruchkov <kruchkovdmitriy48@yandex.ru>
2024-05-11 10:18:52 +0300

    Добавлена функция записи на событие
    Signed-off-by: DmitriyKruchkov <kruchkovdmitriy48@yandex.ru>

--- flaskr/data/__all_models.py
+++ flaskr/data/__all_models.py
@@ -2,3 +2,4 @@ from data.users import User
 from data.events import Event
 from data.event_types import EventType
 from data.roles import Role
+from data.joined_users import JoinedUsers
---
+++ flaskr/services/joined_users.py
@@ -0,0 +1,23 @@
+from sqlalchemy.exc import IntegrityError
+
+from data.joined_users import JoinedUsers
+from database import create_session
+
+
+def getJoinedUsers():
+    db_sess = create_session()
+    return db_sess.query(JoinedUsers).all()
+
+
+def joinUserToEvent(email, event_id):
+    db_sess = create_session()
+    user = JoinedUsers(
+        email=email,
+        event_id=event_id
+    )
+    try:
+        db_sess.add(user)
+        db_sess.commit()
+    except IntegrityError:
+        pass
+    db_sess.close()
